d05688feafe6cd476bcc327cbd86a5dcaeab6c32
Seth <sethclark27@gmail.com>
2023-08-28 01:39:03 -0600

    intrapage nav faded highlight

--- portfolioSC.js
+++ portfolioSC.js
@@ -4,3 +4,15 @@
  * and open the template in the editor.
  */
 window.addEventListener("hashchange", function() { scrollBy(0, -100); });
+
+
+$(document).ready(function() {
+    $(".btn-nav").click(function() {
+        var target = $(this).data("target");
+        $(target).addClass("faded-highlight");
+
+        setTimeout(function() {
+            $(target).removeClass("faded-highlight");
+        }, 1000); // pulse length
+    });
+});
